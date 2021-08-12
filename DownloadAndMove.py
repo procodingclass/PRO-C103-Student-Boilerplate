78cbb80823cefb3d9ac4ff22b8cfa924b63f73e9
PreetiSharma15 <pwhitehat6@gmail.com>
2021-08-12 21:00:37 +0530

    Updated

--- DownloadAndMove.py
+++ DownloadAndMove.py
@@ -8,9 +8,11 @@ import shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-from_dir = "ENTER THE PATH OF DOWNLOAD FOLDER (USE "/ ") in VSC"
-to_dir = "ENTER THE PATH OF DESTINATION FOLDER(USE "/ ") in VSC"
+# from_dir = "ENTER THE PATH OF DOWNLOAD FOLDER (USE " / ") in VSC"
+# to_dir = "ENTER THE PATH OF DESTINATION FOLDER(USE " / ") in VSC"
 
+from_dir = "C:/Users/preet/Downloads"
+to_dir = "C:/Users/preet/Desktop/Downloaded_Files"
 
 dir_tree = {
     "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
@@ -19,37 +21,32 @@ dir_tree = {
     "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
 }
 
+# Event Hanlder Class
 
 class FileMovementHandler(FileSystemEventHandler):
 
     def on_created(self, event):
-       
-        name, extension = os.path.splitext(event.src_path)       
-        time.sleep(1)
-        print("name :" + name)
-        print("extension :" + extension)
+        print(event)
+        print(event.src_path)
 
 
-
-# Initialize event handler
+# Initialize Event Handler Class
 event_handler = FileMovementHandler()
 
 
 # Initialize Observer
 observer = Observer()
 
+# Schedule the Observer
 observer.schedule(event_handler, from_dir, recursive=True)
 
 
 # Start the Observer
 observer.start()
 
-try:
-    while True:
-        time.sleep(2)
-        print("running...")
-except KeyboardInterrupt:
-    print("stopped")
-    observer.stop()
 
-observer.join()
+while True:
+    time.sleep(2)
+    print("running...")
+
+    
